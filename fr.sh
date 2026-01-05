d0697bff0fbe82b43a6badabfe2183a9a693620a
OM262006 <omyengantiwar26@gmail.com>
2026-01-05 20:02:47 +0530

    add shell script

---
+++ fr.sh
@@ -0,0 +1,9 @@
+  GNU nano 7.2                                                                                 3.sh                                                                                           
+#!/bin/bash 
+name="om"
+age=19
+
+echo "Name: $name"
+echo "Age:  $age"
+
+
